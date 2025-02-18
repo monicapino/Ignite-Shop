b51cb3f98204c1da5eb3a5af0f71f75d9adab937
Monica Pino <112265101+monicapino@users.noreply.github.com>
2025-02-18 13:16:01 -0300

    Implement success structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/sucess";
+
 export default function Success() {
     return (
-        <h1>Success</h1>
+        <SuccessContainer>
+            <h1>Conta criada com sucesso!</h1>
+
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <p>
+                Uhuul <strong>Neymar</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa. 
+            </p>
+
+            <Link href="/">
+                Voltar ao catálogo
+            </Link>
+        </SuccessContainer>
     )
 }
